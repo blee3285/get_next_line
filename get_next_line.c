f9ebd0dad1bfe7f1940abbbf4c0b7f91e684a552
Benson Lee <blee@e1z2r5p1.42.us.org>
2017-04-18 18:49:57 -0700

    drafting

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,25 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 15:43:37 by blee              #+#    #+#             */
-/*   Updated: 2017/04/18 18:25:25 by blee             ###   ########.fr       */
+/*   Updated: 2017/04/18 18:49:08 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int		find_newline(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+		{
+			*str = '\0';
+			return (1);
+		}
+		str++;
+	}
+	return (0);
+}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -19,8 +33,16 @@ int		get_next_line(const int fd, char **line)
 
 	if (!(buff = (char *)malloc(sizeof(*buff) * (BUFF_SIZE + 1))))
 		return (-1);
-	while (reading = read(fd, buf, BUFF_SIZE))
+	while (reading = read(fd, buff, BUFF_SIZE))
 	{
-
+		buff[reading] = '\0';
+		if (find_newline)
+		{
+			ft_strcat(*line, buff);
+			return (1);
+		}
+		else
+			ft_strcat(*line, buff);
 	}
+	return (0);
 }
