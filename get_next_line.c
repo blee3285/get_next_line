4d84d2eff9809ebd7b116475b61a92491a4bb2fa
Benson Lee <blee@e1z2r8p1.42.us.org>
2017-04-22 18:53:16 -0700

    new idea

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,49 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 15:43:37 by blee              #+#    #+#             */
-/*   Updated: 2017/04/20 22:14:28 by blee             ###   ########.fr       */
+/*   Updated: 2017/04/22 18:50:35 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+int		bigger_buff(char **str, int	size)
+{
+	char	*temp;
+
+	temp = ft_strnew(size);
+	if (!temp)
+		return (1);
+	temp = ft_strcpy(temp, *str);
+	free(*str);
+	*str = temp;
+	return (0);
+}
+
+int		read_file(const int fd, char **str)
+{
+	int		size;
+	int		ret;
+	char	*buff;
+
+	size = 1024;
+	buff = ft_strnew(BUFF_SIZE + 1);
+	*str = ft_strnew(size);
+	if (!buff || !*str)
+		return (1);
+	while ((ret = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[ret] = '\0';
+		if ((ft_strlen(*str) + ret) > size)
+		{
+			while ((ft_strlen(*str) + ret) > size)
+				size *= 2;
+			if (bigger_buff(str, size))
+				return (1);
+		}
+		ft_strcat(*str, buff);
+	}
+	return (0);
+}
 
 int		find_newline(char *str)
 {
@@ -31,59 +69,28 @@ void	next_str(char *str)
 	int i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 		i++;
 	i++;
 	if (str[i])
 		ft_strcpy(str, &str[i]);
-	else
-		ft_bzero(str, BUFF_SIZE + 1);
-}
-
-int		check_buff(char *buff, char *line)
-{
-	if (*buff)
-	{
-		if (find_newline(buff))
-		{
-			ft_strcat(line, buff);
-			next_str(buff);
-			return (1);
-		}
-		ft_strcat(line, buff);
-	}
-	return (0);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char	*buff;
-	int			reading;
+	char		buff[BUFF_SIZE + 1];
+	static char	*hold[1024];
 
-
-	if (!*line)
-		*line = ft_strnew(1);
-	**line = 0;
-	if (!buff)
-		buff = ft_strnew(BUFF_SIZE + 1);
-	if (check_buff(buff, *line))
-		return (1);
-	while ((reading = read(fd, buff, BUFF_SIZE)))
+	if (fd < 0 || !line || BUFF_SIZE == 0 || fd >= 1024)
+		return (-1);
+	if (!hold[fd])
+		if (read_file(fd, &hold[fd]))
+			return (-1);
+	if (find_newline(hold[fd]))
 	{
-		buff[reading] = '\0';
-		if (find_newline(buff))
-		{
-			ft_strcat(*line, buff);
-			next_str(buff);
-			return (1);
-		}
-		else
-		{
-			ft_strcat(*line, buff);
-			next_str(buff);
-		}
-	}
-	if (**line)
+		*line = ft_strdup(hold[fd]);
+		next_str(hold[fd]);
 		return (1);
+	}
 	return (0);
 }
--- main.c
+++ main.c
@@ -5,14 +5,15 @@ int main(int ac, char **av)
 	char    *line;
 	int fd1;
 	int i;
+	int ret;
 
 	fd1 = open(av[1], O_RDONLY);
 	//line = ft_strnew(500);
 	line = NULL;
 	i = 0;
-	while (get_next_line(fd1, &line))
+	while ((ret = get_next_line(fd1, &line)) && i < 10)
 	{
-		ft_putnbr(i);
+		ft_putnbr(ret);
 		ft_putstr(": ");
 		ft_putstr(line);
 		ft_putchar('\n');
